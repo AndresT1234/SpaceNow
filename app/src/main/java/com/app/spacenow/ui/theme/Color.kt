d91e7294fe0fd55f7583f28ed42827faaa7e39de
AlejoGomezQ <alejandro.gomezq97@gmail.com>
2025-04-21 18:16:33 -0500

    feat: add color palette

--- app/src/main/java/com/app/spacenow/ui/theme/Color.kt
+++ app/src/main/java/com/app/spacenow/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightGray = Color(0xFFE4E0E1)
+val BeigeLight = Color(0xFFD6C0B3)
+val BeigeDark = Color(0xFFAB886D)
+val Brown = Color(0xFF493628)
+
+// Estado colors
+val AvailableGreen = Color(0xFF4CAF50) // Color verde para el badge de disponible
